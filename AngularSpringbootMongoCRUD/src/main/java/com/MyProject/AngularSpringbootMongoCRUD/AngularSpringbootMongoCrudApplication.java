262d5c509f8b852a055c8feaa229a051aeb9ec58
Andres <andrarias@gmail.com>
2022-11-17 14:19:30 -0300

    Ininitial

---
+++ AngularSpringbootMongoCRUD/src/main/java/com/MyProject/AngularSpringbootMongoCRUD/AngularSpringbootMongoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.MyProject.AngularSpringbootMongoCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootMongoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootMongoCrudApplication.class, args);
+	}
+
+}
---
+++ AngularSpringbootMongoCRUD/src/test/java/com/MyProject/AngularSpringbootMongoCRUD/AngularSpringbootMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProject.AngularSpringbootMongoCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringbootMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
